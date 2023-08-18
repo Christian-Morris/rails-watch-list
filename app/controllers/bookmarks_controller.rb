83e6be2b9f04b173722fcb1ca2563ca61765d5c6
Christian Morris <cjmorris96@hotmail.com>
2023-08-18 13:19:50 +0800

    Added bookmark deletion functionality

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,29 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark[:list_id] = @list.id
+    if @bookmark.save
+      redirect_to list_path(params[:list_id]) #need to find out what param this is linking to, the link id...
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    @bookmark.destroy
+    redirect_to list_path(params[:id])
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
